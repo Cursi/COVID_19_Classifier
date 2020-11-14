733431120c93588650955e09d35094cac412e4e6
Cursi <razvan.diib@yahoo.com>
2020-11-14 04:03:14 +0200

    Added simple file upload passed to python

--- dummy.py
+++ dummy.py
@@ -1,1 +1,5 @@
-print("Muhahahaaaaa")
+import sys
+
+if __name__ == "__main__":
+    print("Hello from python run with arguments:")
+    print(sys.argv)
--- server.js
+++ server.js
@@ -2,19 +2,46 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-var cmd = require("node-cmd");
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
+
+const pyCmd = require("node-cmd");
 
 app.get('/', (req, res) => 
 {
-    const pyProg = cmd.run("python dummy.py").stdout.on('data', function(data) 
-    {
-        console.log(data.toString());
-        res.write(data);
-        res.end();
-    });
+    res.sendFile(__dirname + "/index.html");
 })
+
+app.post("/upload", (req, res) =>
+{
+    console.log(req.files);
+
+    if(req.files)
+    {
+        let file = req.files.dataset;
+        let filename = file.name;
+
+        file.mv("./upload/" + filename, (err) =>
+        {
+            if(err)
+            {
+                console.log(err);
+                res.send(err);
+            }
+            else
+            {
+                pyCmd.run(`python dummy.py ./upload/${filename}`).stdout.on('data', function(data) 
+                {
+                    console.log(data.toString());
+                    res.write(data);
+                    res.end();
+                });
+            }
+        });
+    }
+});
 
 app.listen(port, () => 
 {
-    console.log(`Example app listening on ${port}`)
+    console.log(`Example app listening on ${port}`);
 })
