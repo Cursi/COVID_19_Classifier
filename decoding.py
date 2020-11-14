ce0e71d87dfe3c1e51384e602f0a673c6d2bae06
Cursi <razvan.diib@yahoo.com>
2020-11-14 16:40:39 +0200

    Added pandas dependency

--- decoding.py
+++ decoding.py
@@ -2,15 +2,17 @@ import io
 import base64
 import pandas as pd
 
-base64_dataset = input()
-decrypted_dataset = base64.b64decode(base64_dataset)
-# print(decrypted_dataset)
+print(pd.__version__)
 
-toread = io.BytesIO()
-toread.write(decrypted_dataset)  # pass your `decrypted` string as the argument here
-toread.seek(0)  # reset the pointer
+# base64_dataset = input()
+# decrypted_dataset = base64.b64decode(base64_dataset)
+# # print(decrypted_dataset)
 
-df = pd.read_excel(toread)  # now read to dataframe
-print(df.shape)
+# toread = io.BytesIO()
+# toread.write(decrypted_dataset)  # pass your `decrypted` string as the argument here
+# toread.seek(0)  # reset the pointer
 
-df.to_excel("test.xlsx")  
+# df = pd.read_excel(toread)  # now read to dataframe
+# print(df.shape)
+
+# df.to_excel("test.xlsx")  
