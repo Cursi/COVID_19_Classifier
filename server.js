0a3c790ef32e87fcbf2400750f7688211155623a
Cursi <razvan.diib@yahoo.com>
2020-11-14 03:23:36 +0200

    Added python runtime and reverted node py module

--- server.js
+++ server.js
@@ -2,34 +2,16 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-// var cmd=require('node-cmd');
-const PS = require('python-shell');
+var cmd = require("node-cmd");
 
 app.get('/', (req, res) => 
 {
-    // const { spawn } = require('child_process');
-    // // const pyProg = spawn('python', ['./dummy.py']);
-    // const pyProg = spawn('heroku', ['run', 'python', './dummy.py']);
-
-    // const pyProg = cmd.run("python dummy.py")
-
-    PS.PythonShell.run('dummy.py', null, function (err) 
-    {
-        if (err) throw err;
-            console.log('finished');
-    }).stdout.on('data', function(data) 
+    const pyProg = cmd.run("python dummy.py").stdout.on('data', function(data) 
     {
         console.log(data.toString());
         res.write(data);
         res.end();
     });
-
-    // pyProg.stdout.on('data', function(data) 
-    // {
-    //     console.log(data.toString());
-    //     res.write(data);
-    //     res.end();
-    // });
 })
 
 app.listen(port, () => 
