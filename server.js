c3c8b318e5195be0fe7ade932519c5a02a5ece54
Cursi <razvan.diib@yahoo.com>
2020-11-14 03:13:04 +0200

    Trying python shell from node

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-var cmd=require('node-cmd');
+// var cmd=require('node-cmd');
+const PS = require('python-shell');
 
 app.get('/', (req, res) => 
 {
@@ -10,14 +11,25 @@ app.get('/', (req, res) =>
     // // const pyProg = spawn('python', ['./dummy.py']);
     // const pyProg = spawn('heroku', ['run', 'python', './dummy.py']);
 
-    const pyProg = cmd.run("python dummy.py")
+    // const pyProg = cmd.run("python dummy.py")
 
-    pyProg.stdout.on('data', function(data) 
+    PS.PythonShell.run('dummy.py', null, function (err) 
+    {
+        if (err) throw err;
+            console.log('finished');
+    }).stdout.on('data', function(data) 
     {
         console.log(data.toString());
         res.write(data);
         res.end();
     });
+
+    // pyProg.stdout.on('data', function(data) 
+    // {
+    //     console.log(data.toString());
+    //     res.write(data);
+    //     res.end();
+    // });
 })
 
 app.listen(port, () => 
